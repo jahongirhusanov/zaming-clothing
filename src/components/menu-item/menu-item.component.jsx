23b13115a5d79f445ec3f227fd63bc7d940144b4
jahongirhusanov <husanov.y.jahongir@gmail.com>
2021-07-04 17:22:09 -0500

    to hooks

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import './menu-item.style.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      className='background-image'
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
+// const MenuItem = ({ title, imageUrl, size }) => (
+//   <div className={`${size} menu-item`}>
+//     <div
+//       className='background-image'
+//       style={{ backgroundImage: `url(${imageUrl})` }}
+//     />
+//     <div className='content'>
+//       <h1 className='title'>{title.toUpperCase()}</h1>
+//       <span className='subtitle'>SHOP NOW</span>
+//     </div>
+//   </div>
+// )
+
+function MenuItem({ title, imageUrl, size }) {
+  return (
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default MenuItem
